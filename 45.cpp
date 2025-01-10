3fb80905107fc97fb3adc5f3a463ada8bbbe2a17
Sameer  Moyal <sameerab827@gmail.com>
2025-01-10 22:42:02 +0530

    more

---
+++ 45.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+void alphabet(int n)
+{
+    
+    int num = 65;
+    for (int i = 0; i < n; i++) {
+
+        
+        for (int j = 0; j <= i; j++) {
+
+          
+            char ch = char(num);
+
+           
+            cout << ch << " ";
+        }
+
+       
+        num = num + 1;
+
+        
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    int n = 5;
+    alphabet(n);
+    return 0;
+}
